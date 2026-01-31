7134d524c221866938e5cf2c26facd37cb078fad
anitasundaram <reachanitas@gmail.com>
2026-01-31 21:15:47 +0530

    V3 commit - Full Site

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-// 🔴 REPLACE THIS WITH YOUR DEPLOYED APPSCRIPT WEB APP URL
+// 🔴 INSTRUCTION: Open your AppScript -> Deploy -> Web App -> Copy URL -> Paste below
 const API_CONFIG = {
     URL: "https://script.google.com/macros/s/AKfycbx55y-N_lsJV7mgA8w9naFkNGo0R9TS1ee3l_z--o3PGAvSn8xMNwQ9VVmB8vTzNKj5/exec" 
 };
