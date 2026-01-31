eaaa78de1ae595832468b6f2b03dffbecc8a8e9d
anitasundaram <reachanitas@gmail.com>
2026-01-31 20:49:49 +0530

    V2 commit - Full Site

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+// 🔴 REPLACE THIS WITH YOUR DEPLOYED APPSCRIPT WEB APP URL
+const API_CONFIG = {
+    URL: "https://script.google.com/macros/s/AKfycbx55y-N_lsJV7mgA8w9naFkNGo0R9TS1ee3l_z--o3PGAvSn8xMNwQ9VVmB8vTzNKj5/exec" 
+};
