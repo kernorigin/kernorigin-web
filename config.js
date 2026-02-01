e4d3e9ec3a2eda7f36de9057b127e2623667e791
anitasundaram <reachanitas@gmail.com>
2026-02-01 20:31:32 +0530

    V3.4 claude commit - Full Site

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 // 🔴 INSTRUCTION: Open your AppScript -> Deploy -> Web App -> Copy URL -> Paste below
 const API_CONFIG = {
-    URL: "https://script.google.com/macros/s/AKfycbzkfxakM1RgekUzadBibotgsX4jSIdqa5mKULcRoQyNTql8S_vzTgMU0CGnoUuqB3on/exec" 
+    URL: "https://script.google.com/macros/s/AKfycbzoYb6iEoj5PR-FOpuvrj44RwfzSL_vtIvaH_fa-7smXNKVXwgJtafn7XU_enmCULTx/exec" 
 };
