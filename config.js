708058b744ea8ca526c164a2e9032eff4f721ea8
anitasundaram <reachanitas@gmail.com>
2026-02-08 15:15:41 +0530

    v10 backend

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
 const API_CONFIG = {
     // Your Google Apps Script Web App URL
     // REPLACE THIS with your actual deployment URL
-    URL: 'https://script.google.com/macros/s/AKfycbwCJSGofYZ7e8diVYwF9lmAArJ_z0DBH1VUr5GTZZwoGYMruDYx1tCrV6wcGPVkiCV0/exec',
+    URL: 'https://script.google.com/macros/s/AKfycby3jAEGVkEh6loTYCLuO2bBnXUV6P5EhrWxx6EvXAW47-OqiPzyfHENkddl9RII-9je/exec',
     
     // Your PUBLIC_API_KEY from Script Properties
     // ⚠️ REPLACE THIS with your actual PUBLIC_API_KEY
